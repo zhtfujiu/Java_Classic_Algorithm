969a50b9e676af62631045080d8be30e04aed78c
昊天 <zhaohaotianzht@163.com>
2016-06-13 14:33:57 +0800

    修改了兔子生存月数

--- src/suanfa_1/saunfa_1.java
+++ src/suanfa_1/saunfa_1.java
@@ -8,11 +8,11 @@ package suanfa_1;
  */
 public class saunfa_1 {
     public static void main(String args[]){
-        int i=0;
-        for(i=1;i<=20;i++)
+        int i;
+        for(i=1;i<=10;i++)
             System.out.println(f(i));
     }
-    public static int f(int x) {
+    private static int f(int x) {
         if(x==1 || x==2)
             return 1;
         else
